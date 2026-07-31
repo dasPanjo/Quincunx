3e520b8ac09a1448299e6b53da39e1d46698120b
daspanjo <daspanjo@gmail.com>
2026-07-31 20:16:15 +0200

    cleanup: Better pointer assignment, Error handling improved, clean responsibilities

--- engine/Penjin/src/core/Application.h
+++ engine/Penjin/src/core/Application.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Window.h"
+
 #include <memory>
 
 namespace Penjin {
@@ -11,7 +12,6 @@ namespace Penjin {
         int run();
 
     private:
-        std::unique_ptr<Window> window;
-
+        std::unique_ptr<Window> window_;
     };
 }
